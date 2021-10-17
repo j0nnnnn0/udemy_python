5bdc348746deafc18fcde18d785a581fddb5ead1
j0nnnnn0 <j0nnnnn0@gmail.com>
2021-10-17 15:08:03 +0200

    more tuple

--- Sec5.Lists_and_Tuples/enumerate_example.py
+++ Sec5.Lists_and_Tuples/enumerate_example.py
@@ -1,6 +1,7 @@
 # Python Programming Class - Udemy 12.10.2021
 
 # Lists - enumerate example
+# Tuples - enumerate example
 
 import os
 
@@ -8,8 +9,29 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 cls()
 
-# --
+# -- list
+# parts = "abcdefgh"
+# for index, character in enumerate(parts):
+#     print(index,character)
+# print()
 
-parts = "abcdefgh"
-for index, character in enumerate(parts):
-    print(index,character)
+for index, character in enumerate("abcdefg"):
+    print(index, character)
+
+# -- unpacking a Tuple
+for t in enumerate("abcdefg"):
+    index, character = t
+    print(index, character)
+    
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
+# (3, 'd')
+# (4, 'e')
+# (5, 'f')
+# (6, 'g')
+print()
+
+index, character = (0, 'a')
+print(index)
+print(character)
--- Sec5.Lists_and_Tuples/tuples_intro.py
+++ Sec5.Lists_and_Tuples/tuples_intro.py
@@ -23,29 +23,45 @@ cls()
 # # ('Jonathan', 10, 'Python', 2020)
 
 # ---
+
 welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
 bad = "Bad Company", "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 mettalica = "Ride the Lightning", "Metallica", 1984
 
-print(mettalica)
-# ('Ride the lightning', 'Metallica', 1984) prints a tuble
+# # ---
+# print(mettalica)
+# # ('Ride the lightning', 'Metallica', 1984) prints a tuble
 
-print(mettalica[0])
-print(mettalica[1])
-print(mettalica[2])
-# still use brackets to access the index of a tuple
+# print(mettalica[0])
+# print(mettalica[1])
+# print(mettalica[2])
+# # still use brackets to access the index of a tuple
 
-# Tuples are immutable so cannot change them
-# mettalica[0] = "Master of Puppets"
+# # Tuples are immutable so cannot change them
+# # mettalica[0] = "Master of Puppets"
 
-#   File "c:\Users\Jonat\Documents\code\udemy_python\Sec5.Lists_and_Tuples\tuples_intro.py", line 40, in <module>
-#     mettalica[0] = "Master of Puppets"
-# TypeError: 'tuple' object does not support item assignment
+# #   File "c:\Users\Jonat\Documents\code\udemy_python\Sec5.Lists_and_Tuples\tuples_intro.py", line 40, in <module>
+# #     mettalica[0] = "Master of Puppets"
+# # TypeError: 'tuple' object does not support item assignment
 
-# if we want to change a tuple, we have to transform it to a list
-mettalica2 = list(mettalica) # list function transforms it to a list
-print(mettalica2)
-mettalica2[0] = "Master of Puppets"
-print(mettalica2)
+# # if we want to change a tuple, we have to transform it to a list
+# mettalica2 = list(mettalica) # list function transforms it to a list
+# print(mettalica2)
+# mettalica2[0] = "Master of Puppets"
+# print(mettalica2)
+
+# --- Unpack the Tuple
+title, artist, year = mettalica
+print(title)
+print(artist)
+print(year)
+print()
+
+table = ("Coffee table", 200, 100, 75, 34.50)
+print("area of table: {}".format(table[1] * table[2]))
+name, length, width, hight, depth = table
+print("area of table: {}".format(length * width))
+
+
